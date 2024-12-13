eda62ecf8d59b653304013c96f842d9ffd0bf4ba
sai-krishna-reddy-karri <153792301+sai-krishna-reddy-karri@users.noreply.github.com>
2024-12-12 21:57:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
-        if (err) throw err;
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-      }
-    );
+const {MongoClient} = require("mongodb");
+
+const getDeliData = async (client) =>{
+  const cursor = client.db("deli").collection("deliData").find({});
+  const results = await cursor.toArray();
+  return JSON.stringify(results);
+}
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/api"){
+    const URL = "mongodb+srv://krishna:1111@cluster0.rco2y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const client = new MongoClient(URL);
+    try{
+        await client.connect();
+        console.log("Database is connected sucessfully") ;
+        const deli = await getDeliData(client);
+        res.setHeader("Access-Control-Allow-Origin","*");
+        res.writeHead(200,{"content-type":"application/json"});
+        console.log(deli);
+        res.end(deli);
+    }
+    catch(err){
+      console.log("Error in connecting database",err);
   }
+  finally{
+      await client.close();
+      console.log("Database connection is closed");
+  }
+}
   else if (req.url === '/index.css') {
     fs.readFile(path.join(__dirname, 'public', 'index.css'),
       (err, content) => {
@@ -36,30 +53,17 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   }
-  else if (req.url === '/about') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'about.html'),
-      (err, content) => {
-
-        if (err) throw err;
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-      }
-    );
-  }
-  else if (req.url === '/api') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'db.json'), 'utf-8',
-      (err, content) => {
-
-        if (err) throw err;
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(content);
-      }
-    );
-  }
+  else if (req.url === '/') {
+      fs.readFile(path.join(__dirname, 'public', 'index.html'),
+        (err, content) => {
+          if (err) throw err;
+          res.setHeader("Access-Control-Allow-Origin", "*")
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content);
+        }
+      );
+    }
+    
   else {
     res.end("<h1> 404 nothing is here</h1>");
   }
